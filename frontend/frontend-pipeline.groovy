be7be0e4d605a3dded877f844b924214812445e9
PranavSudhirKotawar <pranavkotawar1112@gmail.com>
2026-02-23 21:09:48 +0530

    deployment pipeline is added

--- frontend/frontend-pipeline.groovy
+++ frontend/frontend-pipeline.groovy
@@ -2,7 +2,22 @@ pipeline{
     agent any
     atages{
         stage("Pull"){
-            
+            steps{
+                git 'https://github.com/PranavKotawar2001/three-tear-project.git'
+            }
+        }
+        stage("Build"){
+            steps{
+                sh '''cd frontend
+                   npm install
+                   npm run build'''
+            }
+        }
+        stage("Move-To-S3"){
+            steps{
+                sh '''cd frontend
+                   aws s3 sync dist/ s3://pranav-backend-bucket-00000000/'''
+            }
         }
     }
 }
